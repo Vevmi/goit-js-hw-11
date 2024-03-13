2a9f305b7c3076819b208e78f4b23cb4063b7d1a
Vevmi <timofey.yastremskiy@gmail.com>
2024-03-13 13:16:13 +0300

    fffff

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import { imgsTemplate } from "./js/render-functions";
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
 
 const lightbox = new SimpleLightbox('.gallery a', { });
 const gallery = document.querySelector('.gallery')
@@ -12,13 +17,26 @@ const formElem = document.querySelector('.search-form')
 formElem.addEventListener('submit', (e) => {
     e.preventDefault();
     const userData = formElem.elements.searchQuery.value
+    if (!userData) {
+                iziToast.error({
+    message: 'Feild should not be empty!'
+});
+        return
+    }
     showLoader()
     searchImages(userData).then(({ data }) => {
         const markup = imgsTemplate(data.hits); 
         gallery.innerHTML = markup
         lightbox.refresh();
+        if (data.hits.length == 0) {
+            iziToast.error({
+    message: 'Sorry, there are no images matching your search query. Please try again!'
+});
+        }
     }).catch(() => {
-        console.log('Try later');
+        iziToast.error({
+    message: 'Enter something'
+});
     }).finally(hideLoader);
 })
 
